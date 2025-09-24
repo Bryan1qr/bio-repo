196b7ec1388d700378b0ba8103bdece373994581
Bryan1qr <bquisper@unjbg.edu.pe>
2025-09-24 08:24:30 -0500

    dataset completado

--- Actualización iucn.R
+++ Actualización iucn.R
@@ -4,8 +4,9 @@ remotes::install_github("IUCN-UK/iucnredlist")
 
 #* Initializing IUCN API:
 library(iucnredlist)
-library(iucnredlist)
+api <- init_api("UJjX7CaSLakyorMtTKxU4w465HPHMB4EQkjX")
 
+taxonomia <- read.csv("data_especies.csv", fileEncoding = "Latin1", sep = ";")
 # función para consultar una especie
 iucn_info <- function(x, api){
   # separar género y especie
@@ -26,7 +27,8 @@ iucn_info <- function(x, api){
       } else {
         tibble(
           name = x,
-          category = db$category[1],  # puede haber varias, tomo la primera
+          iucn_id = db$sis_taxon_id[1],
+          category = db$red_list_category_code[1],  # puede haber varias, tomo la primera
           year = db$year_published[1]
         )
       }
@@ -41,6 +43,19 @@ iucn_info <- function(x, api){
   )
   return(out)
 }
-
-# aplicar a toda la tabla (ejemplo con 1000 especies)
+library(tidyverse)
 resultados <- map_dfr(taxonomia$nombre_cientifico, iucn_info, api = api)
+resultadosxd <- resultados
+names(resultadosxd) <- c("nombre_cientifico", "iucn_id", "iucn_status", "iucn_year")
+resultados2 <- map_dfr(taxonomia$nombre_cientifico[330:340], iucn_info, api = api)
+a <- assessments_by_name(api, genus = "rhea", species = "pennata")
+
+
+dataset_actualizado <- taxonomia %>% mutate(uicn_status = as.character(uicn_status),
+                                            iucn_id = as.integer(iucn_id)) %>%
+  rename(iucn_status = "uicn_status") %>%
+  rows_update(resultadosxd %>% distinct() %>% select(-iucn_year), by = "nombre_cientifico")
+
+
+write.table(dataset_actualizado, "data_especies.csv",
+ sep = ";", row.names = FALSE, fileEncoding = "Latin1")
